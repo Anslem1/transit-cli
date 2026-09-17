3b7aa5568fd75c7766eadafab235d61c4d003fc4
Anslem1 <andrewanslem1@gmail.com>
2026-09-17 21:24:26 +0100

    feat: add goreleaser workflow and configure version injection at build time

--- cmd/version.go
+++ cmd/version.go
@@ -1,23 +1,23 @@
 package cmd
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
-
-var verbose bool
+// Version is the current version of transit-cli, injected at build time via ldflags.
+var Version = "1.1.0"
 
 var versionCmd = &cobra.Command{
-    Use:   "version",
-    Short: "Print the version number of transit-cli",
-    Long:  `Print the version number of transit-cli.`,
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("Transit version 1.0.0")
-    },
+	Use:   "version",
+	Short: "Print the version number of transit-cli",
+	Long:  `Print the version number of transit-cli.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Transit version %s\n", Version)
+	},
 }
 
 func init() {
-    rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(versionCmd)
 }
